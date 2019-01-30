df31fa21b24763f3bbe7c7972a596c8561526802
Terri Regan <contact@terriregan.com>
2019-01-30 08:33:31 -0500

    Debugging

--- src/services/googleAuth.js
+++ src/services/googleAuth.js
@@ -3,6 +3,7 @@ import { OAuth2Client } from 'google-auth-library';
 const client = new OAuth2Client( process.env.GOOGLE_CLIENT_ID );
 
 export const verifyGoogleToken = async token => {
+  console.log( `server : ${process.env.GOOGLE_CLIENT_ID}` );
   const ticket = await client.verifyIdToken( {
     idToken: token,
     audience: process.env.GOOGLE_CLIENT_ID
