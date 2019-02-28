5ce146b0cbeea07c95b275c44802f692cedf8122
Edwin Mah <me@edwinmah.com>
2019-02-28 12:53:06 -0500

    Add videoProjects Query resolver

--- src/resolvers/Video.js
+++ src/resolvers/Video.js
@@ -1,11 +1,7 @@
 export default {
   Query: {
-    videos ( parent, args, ctx ) {
-      return ctx.prisma.videos();
-    },
-
-    video( parent, args, ctx ) {
-      return ctx.prisma.video( { id: args.id } );
+    videoProject ( parent, args, ctx ) {
+      return ctx.prisma.videoProject( { id: args.id } );
     }
   },
 
@@ -18,19 +14,5 @@ export default {
       return video;
     },
 
-    updateVideo ( parent, args, ctx ) {
-      const updates = { ...args };
-      delete updates.id;
-      return ctx.prisma.updateVideo( {
-        data: updates,
-        where: {
-          id: args.id
-        }
-      } );
-    },
-
-    deleteVideo( parent, { id }, ctx ) {
-      return ctx.prisma.deleteVideo( { id } );
-    }
-  }
+  Mutation: {}
 };
