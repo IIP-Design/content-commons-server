96dd5a5a2cbafba1baf20c251d4eb1346b7c8c53
Tyler Hebenstreit <tyler.hebenstreit@u.group>
2019-10-07 11:16:33 -0700

    Fixing bad lines in CSv. Debugging whitelist issues.

--- src/lib/whitelist/index.js
+++ src/lib/whitelist/index.js
@@ -4,12 +4,18 @@ import path from 'path';
 
 export const isEmailWhitelisted = email => new Promise( resolve => {
   const file = path.join( __dirname, 'email-whitelist.csv' );
+  const emails = [];
   fs.createReadStream( file )
     .pipe( csv() )
     .on( 'data', data => {
       if ( data.email.toLowerCase() === email.toLowerCase() ) {
         resolve( true );
       }
+      emails.push( data.email );
     } )
-    .on( 'end', () => resolve( false ) );
+    .on( 'end', () => {
+      console.log( `email was not found in whitelist: ${email}` );
+      console.log( emails );
+      resolve( false );
+    } );
 } );
