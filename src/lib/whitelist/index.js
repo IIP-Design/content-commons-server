7e813345cd3a5cfadf5ee005e70e57e6b93a7f7f
Tyler Hebenstreit <tyler.hebenstreit@u.group>
2019-10-07 09:48:26 -0700

    Using the absolute path to the email whitelist CSV in an attempt to get it to work on the server.

--- src/lib/whitelist/index.js
+++ src/lib/whitelist/index.js
@@ -1,8 +1,9 @@
 import fs from 'fs';
 import csv from 'csv-parser';
+import path from 'path';
 
 export const isEmailWhitelisted = email => new Promise( resolve => {
-  const file = 'email-whitelist.csv';
+  const file = path.join( __dirname, 'email-whitelist.csv' );
   fs.createReadStream( file )
     .pipe( csv() )
     .on( 'data', data => {
