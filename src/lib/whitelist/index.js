19c1970c537c6eb69e68b177006b88fe99729ada
Tyler Hebenstreit <tyler.hebenstreit@u.group>
2019-10-07 11:19:17 -0700

    Removing debug output.

--- src/lib/whitelist/index.js
+++ src/lib/whitelist/index.js
@@ -4,18 +4,12 @@ import path from 'path';
 
 export const isEmailWhitelisted = email => new Promise( resolve => {
   const file = path.join( __dirname, 'email-whitelist.csv' );
-  const emails = [];
   fs.createReadStream( file )
     .pipe( csv() )
     .on( 'data', data => {
       if ( data.email.toLowerCase() === email.toLowerCase() ) {
         resolve( true );
       }
-      emails.push( data.email );
     } )
-    .on( 'end', () => {
-      console.log( `email was not found in whitelist: ${email}` );
-      console.log( emails );
-      resolve( false );
-    } );
+    .on( 'end', () => resolve( false ) );
 } );
