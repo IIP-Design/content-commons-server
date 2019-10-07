d859cc14f3c2a489c2282512afad7606a5015b91
Tyler Hebenstreit <tyler.hebenstreit@u.group>
2019-10-07 09:00:42 -0700

    Fixing file path for the email whitelist so that it works within the clustered server.

--- src/lib/whitelist/index.js
+++ src/lib/whitelist/index.js
@@ -1,9 +1,8 @@
 import fs from 'fs';
 import csv from 'csv-parser';
-import path from 'path';
 
 export const isEmailWhitelisted = email => new Promise( resolve => {
-  const file = path.join( __dirname, 'email-whitelist.csv' );
+  const file = 'email-whitelist.csv';
   fs.createReadStream( file )
     .pipe( csv() )
     .on( 'data', data => {
