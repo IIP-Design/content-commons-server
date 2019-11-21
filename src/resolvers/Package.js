4799bb8b8eefc81dbc362e538600e69b2c4ee20c
Edwin Mah <me@edwinmah.com>
2019-11-21 13:49:32 -0500

    Add package field resolvers

--- src/resolvers/Package.js
+++ src/resolvers/Package.js
@@ -7,5 +7,25 @@ export default {
     package ( parent, args, ctx ) {
       return ctx.prisma.package( { id: args.id } );
     },
+  },
+
+  Package: {
+    categories( parent, args, ctx ) {
+      return ctx.prisma
+        .package( { id: parent.id } )
+        .categories( { ...args } );
+    },
+
+    tags( parent, args, ctx ) {
+      return ctx.prisma
+        .package( { id: parent.id } )
+        .tags( { ...args } );
+    },
+
+    documents( parent, args, ctx ) {
+      return ctx.prisma
+        .package( { id: parent.id } )
+        .documents( { ...args } );
+    }
   }
 };
