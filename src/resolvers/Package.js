cd49c18b19418acbd3348c8ac8851923d6b9d9f6
Edwin Mah <me@edwinmah.com>
2019-11-22 08:11:04 -0500

    Add publish & unpublish package mutations

--- src/resolvers/Package.js
+++ src/resolvers/Package.js
@@ -1,3 +1,12 @@
+import { ApolloError, UserInputError } from 'apollo-server-express';
+
+// temp
+const VIDEO_PACKAGE_FULL = 'VIDEO_PACKAGE_FULL';
+const transformPackage = () => {};
+const publishCreate = () => {};
+const publishUpdate = () => {};
+const publishDelete = () => {};
+
 export default {
   Query: {
     packages ( parent, args, ctx ) {
@@ -33,6 +42,52 @@ export default {
       const updates = { ...args };
       const { data, where } = updates;
       return ctx.prisma.updateManyPackages( { data, where } );
+    },
+
+    async publishPackage( parent, args, ctx ) {
+      // 1. Get data for project to publish from db
+      const pkg = await ctx.prisma.package( { id: args.id } ).$fragment( VIDEO_PACKAGE_FULL );
+      if ( !pkg ) {
+        throw new UserInputError( 'A package with that id does not exist in the database', {
+          invalidArgs: 'id'
+        } );
+      }
+
+      // 2. Transform it into the acceptable elasticsearch data structure
+      const esData = transformPackage( pkg );
+
+      const { status } = pkg;
+
+      // 3. Put on the queue for processing ( not sure we need to await here )
+      if ( status === 'DRAFT' ) {
+        publishCreate( args.id, esData, status );
+      } else {
+        publishUpdate( args.id, esData, status );
+      }
+
+      // 4. Update the project status
+      await ctx.prisma.updatePackage( {
+        data: { status: 'PUBLISHING' },
+        where: args
+      } ).catch( err => {
+        throw new ApolloError( err );
+      } );
+
+      return pkg;
+    },
+
+    async unpublishPackage( parent, args, ctx ) {
+      const pkg = await ctx.prisma.package( args ).$fragment( VIDEO_PACKAGE_FULL );
+      if ( !pkg ) {
+        throw new UserInputError( 'A package with that id does not exist in the database', {
+          invalidArgs: 'id'
+        } );
+      }
+      const { id } = pkg;
+
+      publishDelete( id );
+
+      return pkg;
     },
 
     deletePackage ( parent, { id }, ctx ) {
