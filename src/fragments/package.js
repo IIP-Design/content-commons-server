6c55531e02e7ac9c0c2a701caa91c518d42b5897
Edwin Mah <me@edwinmah.com>
2019-11-25 10:41:47 -0500

    Create DOCUMENT_FILE_FULL fragment

--- src/fragments/package.js
+++ src/fragments/package.js
@@ -108,3 +108,85 @@ export const PACKAGE_FULL = `
     }
   }
 `;
+
+export const DOCUMENT_FILE_FULL = `
+  fragment DOCUMENT_FILE_FULL on DocumentFile {
+    id
+    language { id languageCode locale textDirection displayName nativeName }
+    filetype
+    filename
+    filesize
+    author {
+      id
+      firstName
+      lastName
+      email
+      permissions
+    }
+    team {
+      id
+      name
+      organization
+    }
+    status
+    content {
+      id
+      rawText
+      html
+      markdown
+    }
+    image {
+      id
+      language { id languageCode locale textDirection displayName nativeName }
+      dimensions {
+        height
+        width
+      }
+      alt
+      longdesc
+      caption
+      filename
+      filetype
+      visibility
+      use {
+        id
+        name
+      }
+      md5
+      url
+      signedUrl
+    }
+    url
+    signedUrl
+    visibility
+    url: String
+    use {
+      id
+      name
+    }
+    bureaus {
+      id
+      name
+      abbr
+      offices {
+        id
+        name
+        abbr
+      }
+    }
+    categories {
+      id 
+      translations {
+        name
+        language { id languageCode locale textDirection displayName nativeName }
+      }
+    }
+    tags {
+      id 
+      translations {
+        name
+        language { id languageCode locale textDirection displayName nativeName }
+      }
+    }
+  }
+`;
