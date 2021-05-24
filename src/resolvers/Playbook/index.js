9aef51a3fb741396800d4a8a8fbd615ff82a3677
Terri Regan <7433075+terriregan@users.noreply.github.com>
2021-05-24 13:44:24 -0400

    feat: added playbook resolvers

---
+++ src/resolvers/Playbook/index.js
@@ -0,0 +1,63 @@
+import { ApolloError, UserInputError } from 'apollo-server-express';
+
+import { requiresLogin } from '../../lib/authentication';
+import { getAssetPath } from '../../services/aws/s3';
+
+const PlaybookResolvers = {
+
+  Query: requiresLogin( {
+    playbooks( parent, args, ctx ) {
+      return ctx.prisma.playbooks( { ...args } );
+    },
+
+    playbook( parent, args, ctx ) {
+      return ctx.prisma.playbook( { id: args.id } );
+    },
+  } ),
+
+  Mutation: requiresLogin( {
+    async createPlaybook( parent, args, ctx ) {
+      const { data } = args;
+
+      try {
+        const { id, type } = await ctx.prisma.createPlaybook( { ...data } );
+
+        // Set the S3 assetPath using the new package id
+        const assetPath = getAssetPath( id, type.toLowerCase() );
+
+        return ctx.prisma.updatePlaybook( {
+          data: { assetPath },
+          where: { id },
+        } );
+      } catch ( err ) {
+        throw new ApolloError( err );
+      }
+    },
+  } ),
+
+  Playbook: {
+    author( parent, args, ctx ) {
+      return ctx.prisma.playbook( { id: parent.id } ).author( { ...args } );
+    },
+
+    team( parent, args, ctx ) {
+      return ctx.prisma.playbook( { id: parent.id } ).team( { ...args } );
+    },
+
+    categories( parent, args, ctx ) {
+      return ctx.prisma.playbook( { id: parent.id } ).categories( { ...args } );
+    },
+
+    tags( parent, args, ctx ) {
+      return ctx.prisma.playbook( { id: parent.id } ).tags( { ...args } );
+    },
+
+    supportFiles( parent, args, ctx ) {
+      return ctx.prisma.playbook( { id: parent.id } ).supportFiles( { ...args } );
+    },
+  },
+
+};
+
+
+export default PlaybookResolvers;
